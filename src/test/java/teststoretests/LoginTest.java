daf9ccdcf9a6cfd0702865cb284603552c143ecd
mikov-dev <pnmikov@gmail.com>
2024-05-23 00:11:12 +0300

    test store section reached

--- src/test/java/teststoretests/LoginTest.java
+++ src/test/java/teststoretests/LoginTest.java
@@ -3,6 +3,7 @@ package teststoretests;
 import org.config.BaseTest;
 import org.pages.webstore.TestStoreMainPage;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -26,6 +27,11 @@ public class LoginTest extends BaseTest {
         mainPage.enterPassword("Fury@2024");
         mainPage.submitForm();
         Assert.assertTrue(driver.findElement(mainPage.accountName).getText().contains("Peter Mikov"));
+    }
+
+    @AfterMethod
+    void afterMethod() {
+        driver.quit();
     }
 
 }
